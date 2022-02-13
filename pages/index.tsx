5a6064a876d081f3d51f67622e5d661bb229750a
yeonggyu-lim <yeonggyu.lim@navercorp.com>
2022-02-13 10:38:40 +0900

    프로젝트 설정

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  font-size: 21px;
+  color: gray;
+`;
+
+const index = () => {
+  return <Container>hello world</Container>;
+};
+
+export default index;
