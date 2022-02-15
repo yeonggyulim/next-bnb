6097d98830d4f6658b975f69775c463c7c3ccd57
yeonggyu-lim <yeonggyu.lim@navercorp.com>
2022-02-15 23:37:16 +0900

    feat: 공통 헤더

---
+++ components/auth/SignUpModal.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SignUpModal = () => {
+  return <Container>Sign Up</Container>;
+};
+
+export default SignUpModal;
+
+const Container = styled.div`
+  width: 568px;
+  height: 614px;
+  background-color: white;
+  z-index: 11;
+`;
---
+++ hooks/useModal.tsx
@@ -0,0 +1,68 @@
+import { FC, useRef, useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
+
+const useModal = () => {
+  const [modalOpened, setModalOpened] = useState(false);
+
+  const openModal = () => {
+    setModalOpened(true);
+  };
+
+  const closeModal = () => {
+    setModalOpened(false);
+  };
+
+  const ModalPortal: FC = ({ children }) => {
+    const ref = useRef<Element | null>();
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+      setMounted(true);
+      if (document) {
+        const dom = document.querySelector("#root-modal");
+        ref.current = dom;
+      }
+    }, []);
+
+    if (ref.current && mounted && modalOpened) {
+      return createPortal(
+        <Container>
+          <div
+            className="modal-background"
+            role="presentation"
+            onClick={closeModal}
+          />
+          {children}
+        </Container>,
+        ref.current
+      );
+    }
+    return null;
+  };
+
+  return {
+    openModal,
+    closeModal,
+    ModalPortal,
+  };
+};
+
+export default useModal;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 11;
+  .modal-background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.75);
+  }
+`;
